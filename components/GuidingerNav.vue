643901db8f5acd51afe3501e64812194f5b9f995
Will Guidinger <will@fai2.com>
2021-08-01 22:22:32 -0400

    animated logo

--- components/GuidingerNav.vue
+++ components/GuidingerNav.vue
@@ -1,32 +1,13 @@
 <template>
 	<header>
-		<div id="navButton" ref="navButton" @click="navButtonClick()">
-			<svg
-				id="navButtonOverlay"
-				data-name="Layer_1"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 45.23 45.23"
+		<div
+			class="navButton"
+			:class="{ xlose: navState }"
+			ref="navButton"
+			@click="navButtonClick()"
 			>
-				<title>navButton</title>
-				<g class="menuButton">
-					<line
-						ref="line1"
-						id="line1"
-						class="cls-1"
-						y1="18.46"
-						x2="63.03"
-						y2="18.46"
-					/>
-					<line
-						ref="line2"
-						id="line2"
-						class="cls-1"
-						y1="0.47"
-						x2="63.03"
-						y2="0.47"
-					/>
-				</g>
-			</svg>
+			<div class="line line1" id="line1"></div>
+			<div class="line line2" id="line2"></div>
 		</div>
 
 		<div class="navOverlay">
@@ -79,19 +60,12 @@ export default {
 			navState: 'navigation/getNavState'
 		}),
 		navButtonClick() {
-			this.toggle();
+			this.toggle()
+
 			if (process.client) {
 				const nonNavs = document.querySelectorAll('.nonNav')
 				const mainContent = document.querySelectorAll('.mainContent')[0]
-				const navOverlay = document.querySelector('.navOverlay')
 				const container = document.querySelectorAll('.container')[0]
-				// const backgroundImg = document.querySelector('.backgroundImg')
-				const backgroundImgWrap = document.querySelector('.backgroundImgWrap')
-				if (this.nav === false) {
-					line1.setAttribute('x1', '0.33')
-					line1.setAttribute('y1', '0.33')
-					line1.setAttribute('x2', '44.9')
-					line1.setAttribute('y2', '44.9')
 
 					line2.setAttribute('x1', '44.9')
 					line2.setAttribute('y2', '44.9')
@@ -111,15 +85,6 @@ export default {
 
 					this.nav = true
 				} else {
-					line1.removeAttribute('x1')
-					line1.setAttribute('y1', '20.48')
-					line1.setAttribute('x2', '63.03')
-					line1.setAttribute('y2', '20.48')
-
-					line2.removeAttribute('x1')
-					line2.setAttribute('y1', '0.47')
-					line2.setAttribute('x2', '63.03')
-					line2.setAttribute('y2', '0.47')
 					// let container = document.getElementsByClassName("containerNav")[0];
 					container.classList.remove('containerNavShown')
 					// container.classList.add('container');
@@ -132,15 +97,9 @@ export default {
 							nonNavs[i].classList.remove('goodbye')
 						}
 					}
-
-					this.nav = false
 				}
 			}
 		},
 	},
 }
 </script>
-
-<style lang="scss">
-@import '@/assets/scss/_nav.scss';
-</style>
