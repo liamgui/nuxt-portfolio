6a62d71349950887ee701b93eaa4da726355801c
Will Guidinger <will@fai2.com>
2021-08-01 22:23:05 -0400

    nav converting and cleanup

--- components/GuidingerNav.vue
+++ components/GuidingerNav.vue
@@ -5,13 +5,16 @@
 			:class="{ xlose: navState }"
 			ref="navButton"
 			@click="navButtonClick()"
-			>
+		>
 			<div class="line line1" id="line1"></div>
 			<div class="line line2" id="line2"></div>
 		</div>
 
-		<div class="navOverlay">
-			<div class="backgroundImgWrap">
+		<div ref="navOverlay" class="navOverlay" >
+			<div
+				ref="backgroundImgWrap"
+				class="backgroundImgWrap"
+			>
 				<div class="backgroundImg"></div>
 				<div class="backgroundNav"></div>
 			</div>
@@ -39,25 +42,25 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
+import { mapActions, mapGetters } from 'vuex'
 export default {
 	data() {
-		return {
-			nav: false,
-		}
+		return {}
 	},
 	computed: {
-		// navState() {
-		// 	return this.getNavState()
-		// }
+		navState() {
+			return this.getNavState()
+		},
 	},
-	created() {},
+	created() {
+		// console.log(line1);
+	},
 	methods: {
 		...mapActions({
-			toggle: 'navigation/toggleNavState'
+			toggle: 'navigation/toggleNavState',
 		}),
 		...mapGetters({
-			navState: 'navigation/getNavState'
+			getNavState: 'navigation/getNavState',
 		}),
 		navButtonClick() {
 			this.toggle()
@@ -67,10 +70,9 @@ export default {
 				const mainContent = document.querySelectorAll('.mainContent')[0]
 				const container = document.querySelectorAll('.container')[0]
 
-					line2.setAttribute('x1', '44.9')
-					line2.setAttribute('y2', '44.9')
-					line2.setAttribute('x2', '0.33')
-					line2.setAttribute('y1', '0.33')
+				const navOverlay = this.$refs.navOverlay
+				const backgroundImgWrap = this.$refs.backgroundImgWrap
+				if (this.navState === true) {
 					// container.classList.remove('container');
 					container.classList.add('containerNavShown')
 					navOverlay.classList.add('navShown')
@@ -82,8 +84,6 @@ export default {
 					for (let i = 0; i < nonNavs.length; i++) {
 						nonNavs[i].classList.add('goodbye')
 					}
-
-					this.nav = true
 				} else {
 					// let container = document.getElementsByClassName("containerNav")[0];
 					container.classList.remove('containerNavShown')
